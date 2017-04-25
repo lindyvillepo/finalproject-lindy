6701b800471ec7245eefa85b4682987a324a7f97
Lindy Villeponteau <lvillepo@umich.edu>
2017-04-25 13:26:29 -0700

    finished loading database

--- 206_final_proj.py
+++ 206_final_proj.py
@@ -39,7 +39,7 @@ def get_tweets(user_input):
 		twitter_results = CACHE_DICTION[search]
 	else:
 		print('getting data from internet for', user_input)
-		twitter_results = api.search(q=user_input, count=10, lang="en", result_type="popular")
+		twitter_results = api.search(q=user_input, count=10, lang="en", result_type="mixed")
 		CACHE_DICTION[search] = twitter_results
 		f = open(CACHE_NAME, 'w')
 		f.write(json.dumps(CACHE_DICTION))
@@ -84,9 +84,6 @@ class Movies(object):
 		language_list = movie_diction["Language"].split(", ")
 		return len(language_list)
 
-	def movie_tuple(self):
-		movie_tup = zip(self.id, self.title, self.director, self.languages, self.rating, self.actors)
-
 	def __str__(self):
 		return "{} is directed by {} with a rating of {} with main actor {}.\n".format(self.title, self.director, self.rating, self.actors[0])
 
@@ -99,6 +96,18 @@ class Tweet(object):
 		self.retweets = self.num_retweets(tweet_diction)
 		self.idstr = self.tweet_idstr(tweet_diction)
 		self.search = actor
+
+	def list_of_tweet_tuples(self, tweet_diction, actor):
+		tweet_list = []
+		for tweet in tweet_diction["statuses"]:
+			user_id = tweet["id"]
+			text = tweet["text"]
+			favs = tweet["favorite_count"]
+			retweets = tweet["retweet_count"]
+			post = tweet["user"]["screen_name"]
+			search = actor
+			tweet_list.append((user_id, text, favs, retweets, post, search))
+		return tweet_list
 
 	def tweet_id(self, tweet_diction):
 		id_list = []
@@ -129,12 +138,8 @@ class Tweet(object):
 		for tweet in tweet_diction["statuses"]:
 			idstr_list.append(tweet["id_str"])
 
-	def tweet_tuple(self):
-		tweet_tup = zip(self.id, self.text, self.favorites, self.retweets, self.idstr, self.search)
-		return tweet_tup
-
 	def __str__(self):
-		return "Searching popular tweets about {} with tweets like {}.\n".format(self.search, self.text[0])
+		return "Searching tweets about {} with tweets like {}.\n".format(self.search, self.text[0])
 
 # Write a tweetUser class that takes in a dictionary representing a tweet. The constructor should have information regarding each of the users in the "neighborhood", which is generally defined by tweet posters, all the users mentioned, all the users who have favorited/liked a tweet. Some people also define it with e.g. everyone who has RT'd the tweet as well. For each user, the constructor should initialize the user id, screen name, number of favorites ever favorited, and user description.
 class TweetUser(object):
@@ -144,49 +149,61 @@ class TweetUser(object):
 		self.num_favs = self.favs_ever(tweet_diction)
 		self.description = self.user_description(tweet_diction)
 
+	def list_of_user_tuple(self, tweet_diction):
+		user_list = []
+		for tweet in tweet_diction["statuses"]:
+			user_id = tweet["user"]["id_str"]
+			user_name = tweet["user"]["screen_name"]
+			user_favs = tweet["user"]["favourites_count"]
+			user_desc = tweet["user"]["description"]
+			user_list.append((user_id, user_name, user_favs, user_desc))
+			for user in tweet["entities"]["user_mentions"]:
+				info = api.get_user(id=user["screen_name"])
+				strid = info["id_str"]
+				name = info["screen_name"]
+				favs = info["favourites_count"]
+				desc = info["description"]
+				user_list.append((strid, name, favs, desc))
+		return user_list
+
 	def user(self, tweet_diction):
 		user_list = []
 		for tweet in tweet_diction["statuses"]:
+			user_list.append(tweet["user"]["id_str"])
 			for user in tweet["entities"]["user_mentions"]:
-				info = api.get_user(id=user["id_str"])
+				info = api.get_user(id=user["screen_name"])
 				user_list.append(info["id_str"])
-			for user in tweet["user"]:
-				user_list.append(user["id_str"])
 		return user_list
 
 	def user_name(self, tweet_diction):
 		name_list = []
 		for tweet in tweet_diction["statuses"]:
+			name_list.append(tweet["user"]["screen_name"])
 			for user in tweet["entities"]["user_mentions"]:
-				info = api.get_user(id=user["id_str"])
+				info = api.get_user(id=user["screen_name"])
 				name_list.append(info["screen_name"])
-			for user in tweet["user"]:
-				name_list.append(user["screen_name"])
 		return name_list
 
 	def favs_ever(self, tweet_diction):
 		favs_list = []
 		for tweet in tweet_diction["statuses"]:
+			favs_list.append(tweet["user"]["favourites_count"])
 			for user in tweet["entities"]["user_mentions"]:
-				info = api.get_user(id=user["id_str"])
+				info = api.get_user(id=user["screen_name"])
 				favs_list.append(info["favourites_count"])
-			for user in tweet["user"]:
-				favs_list.append(user["favourites_count"])
 		return favs_list
 
 	def user_description(self, tweet_diction):
 		description_list = []
 		for tweet in tweet_diction["statuses"]:
+			description_list.append(tweet["user"]["description"])
 			for user in tweet["entities"]["user_mentions"]:
-				info = api.get_user(id=user["id_str"])
+				info = api.get_user(id=user["screen_name"])
 				description_list.append(info["description"])
-			for user in tweet["user"]:
-				description_list.append(user["description"])
 		return description_list
 
-	def user_tuple(self):
-		user_tup = zip(self.idstr, self.screen_name, self.num_favs, self.description)
-		return user_tup
+	def __str__(self):
+		return "User {} has made {} favorites ever.\n".format(self.screen_name[0], self.num_favs[0])
 
 # Write a list of the three movie search terms you want to use to search in OMDB and put them in a list called movie_search_list. Then, make a request to OMDB on each of those search terms and accumulate the dictionaries of each movie into a list called movie_info_list.
 movie_search_list = ["the proposal", "neighbors 2", "crazy, stupid, love"]
@@ -201,19 +218,16 @@ for movie in movie_info_list:
 	instance = Movies(movie)
 	movie_instance.append(instance)
 
-# Iterate through movie_instance to get the twitter diction of the second top-paid actor. Then get an instance of that tweet and append it in the list "twitter_list"
+# Iterate through movie_instance to get the twitter diction of the second top-paid actor. Then get an instance of that tweet and the users and append it in the list "twitter_list" and "user_list". 
 twitter_list = []
 user_list = []
 for movie in movie_instance:
 	actor = movie.actors[1]
-	print(actor)
 	tweet_diction = get_tweets(actor)
 	tweet = Tweet(tweet_diction, actor)
-	print(tweet)
-	twitter_list.append(tweet)
+	twitter_list.append(tweet.list_of_tweet_tuples(tweet_diction, actor))
 	user = TweetUser(tweet_diction)
-	print(user_instance)
-	user_list.append(user)
+	user_list.append(user.list_of_user_tuple(tweet_diction))
 
 ## Part 2 - Creating database and loading data into database
 
@@ -241,7 +255,7 @@ cur.execute(user_table)
 # -actor - containing the string of the movie's top paid actor/actress
 cur.execute('DROP TABLE IF EXISTS Movies')
 movie_table = 'CREATE TABLE IF NOT EXISTS '
-movie_table += 'Movies (movie_id INTEGER PRIMARY KEY, title TEXT, director TEXT, languages INTEGER, imbd_rating INTEGER, actor TEXT)'
+movie_table += 'Movies (movie_id TEXT PRIMARY KEY, title TEXT, director TEXT, languages INTEGER, imbd_rating DECIMAL, actor TEXT)'
 cur.execute(movie_table)
 
 # Table Tweets, with columns:
@@ -250,30 +264,27 @@ cur.execute(movie_table)
 # -favorites - containing the number that represents the number of favorites the tweet had
 # -retweets - containing the number that represents how many times the tweet has been retweeted
 # -user_id - containing an ID string, referencing the Users table
-# -movie_search - containing a string of the movie search this tweet came from (represented by a reference to the movies table)
+# -search - containing a string of the movie search this tweet came from (represented by a reference to the movies table)
 cur.execute('DROP TABLE IF EXISTS Tweets')
 statement = 'CREATE TABLE IF NOT EXISTS '
-statement += 'Tweets (tweet_id INTEGER PRIMARY KEY, tweet_text TEXT, favorites INTEGER, retweets INTEGER, user_id TEXT NOT NULL, FOREIGN KEY (user_id) REFERENCES Users(user_id) on UPDATE SET NULL, movie_search TEXT NOT NULL, FOREIGN KEY (movie_search) REFERENCES Movies(title) on UPDATE SET NULL)'
+statement += 'Tweets (tweet_id INTEGER PRIMARY KEY, tweet_text TEXT, favorites INTEGER, retweets INTEGER, user_post TEXT NOT NULL, search TEXT NOT NULL, FOREIGN KEY (user_post) REFERENCES Users(screen_name) on UPDATE SET NULL, FOREIGN KEY (search) REFERENCES Movies(actor) on UPDATE SET NULL)'
 cur.execute(statement)
 
 # You should load into the Users table:
 # The user, and all of the data about users that are mentioned in the movie search's timeline. 
 
-# user_id = []
-# screen_name = []
-# num_favs = []
-# description = []
-# for user in user_list:
-# 	user_id.append(user.idstr)
-# 	screen_name.append(user.screen_name)
-# 	num_favs.append(user.num_favs)
-# 	description.append(user.description)
+user_id = []
+screen_name = []
+num_favs = []
+description = []
+for search in user_list:
+	for user in search:
+		user_id.append(user[0])
+		screen_name.append(user[1])
+		num_favs.append(user[2])
+		description.append(user[3])
 
-# user_info = zip(user_id, screen_name, num_favs, description)
-
-user_info = []
-for user in user_list:
-	user_info.append(movie.user_tuple())
+user_info = zip(user_id, screen_name, num_favs, description)
 
 user_table = 'INSERT OR IGNORE INTO Users VALUES (?, ?, ?, ?)'
 for user in user_info:
@@ -282,25 +293,21 @@ for user in user_info:
 # You should load into the Movies table:
 # Info about the movie that is from the movie search's timeline.
 
-# movie_id = []
-# titles = []
-# directors = []
-# language = []
-# rating = []
-# actor = []
-# for movie in movie_instance:
-# 	movie_id.append(movie.id)
-# 	titles.append(movie.title)
-# 	directors.append(movie.director)
-# 	language.append(movie.languages)
-# 	rating.append(movie.rating)
-# 	actor.append(movie.actors[0])
-
-# movie_info = zip(movie_id, titles, directors, language, rating, actor)
-
-movie_info = []
+movie_id = []
+titles = []
+directors = []
+language = []
+rating = []
+actor = []
 for movie in movie_instance:
-	movie_info.append(movie.movie_tuple())
+	movie_id.append(movie.id)
+	titles.append(movie.title)
+	directors.append(movie.director)
+	language.append(movie.languages)
+	rating.append(movie.rating)
+	actor.append(movie.actors[0])
+
+movie_info = zip(movie_id, titles, directors, language, rating, actor)
 
 movie_table = 'INSERT INTO Movies VALUES (?, ?, ?, ?, ?, ?)'
 for movie in movie_info:
@@ -309,48 +316,107 @@ for movie in movie_info:
 # You should load into the Tweets table: 
 # Info about all the tweets that is from the movie search's timeline.
 
+lol = []
+kms = []
+wtf = []
+fml = []
+smd = []
+fuck = []
+for search in twitter_list:
+	for tweet in search:
+		lol.append(tweet[0])
+		kms.append(tweet[1])
+		wtf.append(tweet[2])
+		fml.append(tweet[3])
+		smd.append(tweet[4])
+		fuck.append(tweet[5])
+
+print(len(lol), len(kms), len(wtf), len(fml), len(smd), len(fuck))
+
 # tweet_id = []
 # text = []
-# num_favorite = []
+# favorites = []
 # retweets = []
-# user_id = []
-# movie_title = []
-# for tweet in twitter_list:
-# 	tweet_id.append(tweet.id)
-# 	tweet_text.append(tweet.text)
-# 	num_favorite.append(tweet.favorites)
-# 	retweets.append(tweet.retweets)
-# 	user_id.append(tweet.idstr)
-# 	movie_title.append(tweet.search)
+# posts = []
+# search = []
+# for search in twitter_list:
+# 	for tweet in search:
+# 		tweet_id.append(tweet[0])
+# 		text.append(tweet[1])
+# 		favorites.append(tweet[2])
+# 		retweets.append(tweet[3])
+# 		print(type(tweet[4]))
+# 		print(tweet[4])
 
-# tweet_info = list(zip(tweet_id, tweet_text, num_favorite, retweets, user_id, movie_title))
+		#posts.append(tweet[4])
 
-tweet_info = []
-for tweet in twitter_list:
-	tweet_info.append(tweet.tweet_tuple())
+tweet_info = zip(lol, kms, wtf, fml, smd, fuck)
 
 statement = 'INSERT INTO Tweets VALUES (?, ?, ?, ?, ?, ?)'
 for tweet in tweet_info:
 	cur.execute(statement, tweet)
 
+conn.commit()
+
 ## Part 3 - Making queries
 
 # Make a query to select all of the tweet texts from the database. Save a resulting list of strings in the variable tweet_texts.
-query = "SELECT tweet_text FROM Tweets"
-cur.execute(query1)
-tuple_text = cur.fetchall()
-tweet_texts = [x[0] for x in tuple_text]
+# query = "SELECT tweet_text FROM Tweets"
+# cur.execute(query1)
+# tuple_texts = cur.fetchall()
+# tweet_texts = [x[0] for x in tuple_texts]
+
+# Make a query using an INNER JOIN to get a list of tuples with 2 elements in each tuple: the user screenname and the text of the tweet -- for each tweet that has been retweeted more than 50 times. Save the resulting list of tuples in a variable called user_description.
+# query1 = "SELECT Tweets.tweet_id, Users.description FROM Tweets INNER JOIN Users on Tweets.user_id = Users.user_id"
+# cur.execute(query1)
+# user_description = cur.fetchall()
+
+# Select all of the TEXT values of the tweets that are retweets of another account (i.e. have "RT" at the beginning of the tweet text). Save the tuples in a variable called tuple_rt.
+# query2 = "SELECT tweet_text FROM Tweets WHERE instr(tweet_text, 'RT')"
+# cur.execute(query2)
+# tuple_rt = cur.fetchall()
 
 ## Part 4 - Manipulating data
 
 # Use a set comprehension to get a set of all words among the texts in the tweet_texts list. Save the resulting set in a variable called tweet_words.
-word_list = []
-for line in tweet_texts:
- 	words = line.split()
- 	for word in words:
- 		word_list.append(word)
+# word_list = []
+# for line in tweet_texts:
+#  	words = line.split()
+#  	for word in words:
+#  		word_list.append(word)
 
-tweet_words = {word for word in word_list}
+# tweet_words = {word for word in word_list}
+
+# Use a Counter in the collections library to find the most common word among all of the texts in the tweet_texts list. Save that most common word in a variable called most_common. 
+#below is for common character
+# word_string = " ".join(word for word in word_list)
+# word_string = word_string.replace(" ", "")
+# most_common_char = collections.Counter(word_string).most_common(1)[0]
+# most_common_char = most_common_char[0]
+
+# Write code to create a dictionary whose keys are Twitter tweet id's and whose associated values are lists of tweet texts that that user posted. You should save the final dictionary in a variable called twitter_info.
+# twitter_default = collections.defaultdict(list)
+
+# for key, value in user_description:
+# 	twitter_default[key].append(value)
+
+# twitter_info = dict(twitter_default)
+
+## Make sure to close database!!!
+# conn.close()
+
+# Define a function get_twitter_hashtags that accepts a string as in put and returns a set of the hashtags of each hashtag that was mentioned in the tweet string. Then iterate through tweet_texts to find all the hashtags in the texts and append it to a list called hashtag_set.
+# def get_twitter_hashtags(user_input):
+# 	phrase = r"\@[A-Za-z0-9]+\_*[A-Za-z0-9_]*"
+# 	hashtags = re.findall(phrase, user_input)
+# 	results = []
+# 	for tags in hashtags:
+# 		results.append(tags[1:])
+# 	return set(results)
+
+# hashtag_set = []
+# for tweet in tweet_texts:
+# 	hashtag_set.append(get_twitter_hashtags(tweet))
 
 # Put your tests here, with any edits you now need from when you turned them in with your project plan.
 # class myTests(unittest.TestCase):
